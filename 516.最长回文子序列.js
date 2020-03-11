e653735c9bcd75e41e329d9d8cb364b5aadc0a13
kaizige <m13675637818@163.com>
2020-03-12 07:21:57 +0800

    最长回文子序列2

--- 516.最长回文子序列.js
+++ 516.最长回文子序列.js
@@ -9,87 +9,80 @@
  * @param {string} s
  * @return {number}
  */
-var longestPalindromeSubseq1 = function(s) {
-    let map = new Map()
-    // 先循环一遍，把每个字符计算到hash表，方便后面用
-    for (let i = 0; i < s.length; i++) {
-        let str = s.charAt(i)
-        if (map.has(str)) {
-            map.set(str, map.get(str).concat([i]))
-        } else {
-            map.set(str, [i])
-        }
+// var longestPalindromeSubseq1 = function(s) {
+//     function longest(left, right) {
+//         if (left === right) {
+//             return 1;
+//         }
+//         if (left > right) {
+//             return 0;
+//         }
+//         if (s.charAt(left) !== s.charAt(right)) {
+//             // 如果不相等，那么找left -> right-1 和 left+1 -> right的更大值
+//             return Math.max(longest(left, right - 1), longest(left + 1, right));
+//         } else {
+//             // 如果左右相等，那么继续找
+//             return longest(left + 1, right - 1) + 2;
+//         }
+//     }
+ 
+//     return longest(0, s.length - 1);
+// };
+ 
+var longestPalindromeSubseq2 = function(s) {
+    let length = s.length
+    if (length === 1 || s === s.split('').reverse().join('')) {
+        return length
     }
-
-    function longest(left, right) {
-        if (left === right) {
-            return 1;
-        }
-        if (left > right) {
-            return 0;
-        }
-        let leftStr = s.charAt(left)
-        if (leftStr !== s.charAt(right)) {
-            // 左右不相等，那么找到和左边相等的right
-            let arr = map.get(leftStr)
-            if (arr.length === 1 || arr.length === 0) {
-                // 长度为1，说明找不到和left相同的字符，那么找下一个
-                return longest(left+1, right)
+    // 初始化长度为0
+    let f = Array.from({ length }, () => Array.from({ length }, () => 0));
+    for (let i = length-1; i >= 0; i--) {
+        f[i][i] = 1;
+        for (let j = i + 1; j < length; j++) {
+            if (s.charAt(i) === s.charAt(j)) {
+                f[i][j] = f[i+1][j-1] + 2
             } else {
-                leftMirror = arr[arr.length - 1]
-                let pickLen = longest(left + 1, leftMirror - 1) + 2
-                let notPickLen = longest(left + 1, right)
-                return Math.max(pickLen, notPickLen)
+                f[i][j] = Math.max(f[i][j-1], f[i+1][j])
             }
-        } else {
-            // 如果左右相等，那么继续找
-            return longest(left + 1, right - 1) + 2
         }
     }
-
-    return longest(0, s.length - 1);
+    return f[0][length-1]
 };
-
-
 var longestPalindromeSubseq = function(s) {
-    function longest(left, right) {
-        if (left === right) {
-            return 1;
-        }
-        if (left > right) {
-            return 0;
-        }
-        let leftStr = s.charAt(left)
-        if (leftStr !== s.charAt(right)) {
-            // 左右不相等，那么找到和左边相等的right
-            let sameRight = right - 1
-            while (sameRight > left && s.charAt(sameRight) !== leftStr) {
-                sameRight--
-            }
-            // 如果sameRight等于left，说明找不到和leftStr相同的字符
-            if (sameRight === left) {
-                // 长度为1，说明找不到和left相同的字符，那么找下一个
-                return longest(left + 1, right)
+    let length = s.length
+    if (length === 1 || s === s.split('').reverse().join('')) {
+        return length
+    }
+    // 初始化长度为1
+    let f = Array.from({ length }, () => 1);
+    for (let i = length-1; i >= 0; i--) {
+        let temp = 0
+        for (let j = i + 1; j < length; j++) {
+            if (s.charAt(i) === s.charAt(j)) {
+                [temp, f[j]] = [f[j], temp + 2]
             } else {
-                let pickLen = longest(left + 1, sameRight - 1) + 2
-                let notPickLen = longest(left + 1, right)
-                return Math.max(pickLen, notPickLen)
+                temp = f[j-1]
+                f[j] = Math.max(f[j-1], f[j])
             }
-        } else {
-            // 如果左右相等，那么继续找
-            return longest(left + 1, right - 1) + 2
         }
     }
-
-    return longest(0, s.length - 1);
-}
-
+    return f[length-1]
+};
 // @lc code=end
-
+ 
 // test
-console.log(longestPalindromeSubseq('axabxb'))
-// console.log(longestPalindromeSubseq('a') === 1)
-// console.log(longestPalindromeSubseq('aa') === 2)
-// console.log(longestPalindromeSubseq('aba') === 3)
-// console.log(longestPalindromeSubseq('abc') === 1)
-// console.log(longestPalindromeSubseq('ababc')===3)
+// console.log(
+//     longestPalindromeSubseq(
+//         'euazbipzncptldueeuechubrcourfpftcebikrxhybkymimgvldiwqvkszfycvqyvtiwfckexmowcxztkfyzqovbtmzpxojfofbvwnncajvrvdbvjhcrameamcfmcoxryjukhpljwszknhiypvyskmsujkuggpztltpgoczafmfelahqwjbhxtjmebnymdyxoeodqmvkxittxjnlltmoobsgzdfhismogqfpfhvqnxeuosjqqalvwhsidgiavcatjjgeztrjuoixxxoznklcxolgpuktirmduxdywwlbikaqkqajzbsjvdgjcnbtfksqhquiwnwflkldgdrqrnwmshdpykicozfowmumzeuznolmgjlltypyufpzjpuvucmesnnrwppheizkapovoloneaxpfinaontwtdqsdvzmqlgkdxlbeguackbdkftzbnynmcejtwudocemcfnuzbttcoew'
+//     )
+// );
+console.log(longestPalindromeSubseq('abxyzcdmba') === 5);
+console.log(longestPalindromeSubseq('abacadaeaf') === 5);
+console.log(longestPalindromeSubseq('cbbd') === 2);
+console.log(longestPalindromeSubseq('bbbab') === 4);
+console.log(longestPalindromeSubseq('axabxb') === 3);
+console.log(longestPalindromeSubseq('a') === 1);
+console.log(longestPalindromeSubseq('aa') === 2);
+console.log(longestPalindromeSubseq('aba') === 3);
+console.log(longestPalindromeSubseq('abc') === 1);
+console.log(longestPalindromeSubseq('ababc') === 3);
