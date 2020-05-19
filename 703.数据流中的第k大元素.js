988b265bf100e2e29b296396aa65e26d196d2646
kaizige <m13675637818@163.com>
2020-05-19 20:30:57 +0800

    数据流中的第K大元素

--- 703.数据流中的第k大元素.js
+++ 703.数据流中的第k大元素.js
@@ -6,14 +6,28 @@
 
 // @lc code=start
 /**
+ * 交换数组元素
+ * @param {number[]} arr 数组
+ * @param {number} i1 元素1
+ * @param {number} i2 元素2
+ */
+function swap(arr, i1, i2) {
+    let temp = arr[i1];
+    arr[i1] = arr[i2];
+    arr[i2] = temp;
+}
+
+/**
  * @param {number} k
  * @param {number[]} nums
  */
-var KthLargest = function(k, nums) {
+var KthLargest = function (k, nums) {
+    // 第一个元素空着
     this.minHeap = [null];
-    this.maxHeap = [null];
+    this.k = k;
+    // 所有元素都添加到最小堆
     for (let i = 0; i < nums.length; i++) {
-        this.add(nums[i])
+        this.add(nums[i]);
     }
 };
 
@@ -21,32 +35,57 @@ var KthLargest = function(k, nums) {
  * @param {number} val
  * @return {number}
  */
-KthLargest.prototype.add = function(val) {
-    this.minHeap.push(val);
-    this.minUp(this.minHeap.length - 1)
+KthLargest.prototype.add = function (val) {
+    if (this.minHeap.length <= this.k) {
+        // 最小堆没满，放到最小堆
+        this.minHeap.push(val);
+        // 上浮最新的元素，维护堆的秩序
+        this.up(this.minHeap.length - 1);
+    } else {
+        if (val > this.minHeap[1]) {
+            // 新的元素比最小堆堆顶大，替换最小堆堆顶
+            this.minHeap[1] = val;
+            // 下沉这个元素，维护堆的秩序
+            this.down(1);
+        }
+        // 否则直接丢弃这个元素
+    }
+    return this.minHeap[1];
 };
-
-KthLargest.prototype.minUp = function(idx) {
+/**
+ * 最小堆的上浮操作
+ * @param {number} idx 要上浮的元素下标
+ */
+KthLargest.prototype.up = function (idx) {
     let parent = Math.floor(idx / 2);
     if (parent >= 1 && this.minHeap[parent] > this.minHeap[idx]) {
         swap(this.minHeap, parent, idx);
-        this.minUp(parent);
+        // 递归上浮
+        this.up(parent);
     }
 };
-KthLargest.prototype.down = function(heap, idx) {
+/**
+ * 最小堆的下沉操作
+ * @param {number} idx 要下沉的元素下标
+ */
+KthLargest.prototype.down = function (idx) {
     let to = idx;
+    // 和左子元素比较
     let left = idx * 2;
-    if (left < heap.length && heap[left]) {
-        swap(heap, parent, idx);
-        this.minUp(parent);
+    if (left < this.minHeap.length && this.minHeap[left] < this.minHeap[to]) {
+        to = left;
+    }
+    // 和右子元素比较
+    let right = idx * 2 + 1;
+    if (right < this.minHeap.length && this.minHeap[right] < this.minHeap[to]) {
+        to = right;
+    }
+    if (to !== idx) {
+        swap(this.minHeap, to, idx);
+        // 递归下沉
+        this.down(to);
     }
 };
-
-function swap(arr, i1, i2) {
-    let temp = arr[i1];
-    arr[i1] = arr[i2];
-    arr[i2] = temp;
-}
 /**
  * Your KthLargest object will be instantiated and called as such:
  * var obj = new KthLargest(k, nums)
@@ -54,3 +93,24 @@ function swap(arr, i1, i2) {
  */
 // @lc code=end
 
+// const kthLargest = new KthLargest(3, [4,5,8,2]);
+
+// console.log(kthLargest.add(3));
+// console.log(kthLargest.add(5));
+// console.log(kthLargest.add(10));
+// console.log(kthLargest.add(9));
+// console.log(kthLargest.add(4));
+
+// const kthLargest = new KthLargest(1, []);
+// console.log(kthLargest.add(-3));
+// console.log(kthLargest.add(-2));
+// console.log(kthLargest.add(-4));
+// console.log(kthLargest.add(0));
+// console.log(kthLargest.add(4));
+
+// const kthLargest = new KthLargest(3,[5,-1]);
+// console.log(kthLargest.add(2));
+// console.log(kthLargest.add(1));
+// console.log(kthLargest.add(-1));
+// console.log(kthLargest.add(3));
+// console.log(kthLargest.add(4));
